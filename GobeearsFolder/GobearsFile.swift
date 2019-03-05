3bb93f44e90634493d5e894bca9bb7887e43264e
Ajay Merchia <ajaymerchia@Ajays-MacBook-Pro.local>
2019-03-04 23:27:43 -0800

    removed test folder

--- GobeearsFolder/GobearsFile.swift
+++
@@ -1,8 +0,0 @@
-//
-//  GobearsFile.swift
-//  ARMDevSuite
-//
-//  Created by Ajay Merchia on 3/4/19.
-//
-
-import Foundation
--- LocalData2.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LocalData2.swift
-//  ARMDevSuite
-//
-//  Created by Ajay Merchia on 3/4/19.
-//
-
-import Foundation
