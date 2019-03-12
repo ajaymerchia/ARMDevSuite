8e2c4cf0bf6a85963832612613052b7ead62b2a7
Ajay Merchia <ajaymerchia@airbears2-10-142-66-93.airbears2.1918.berkeley.edu>
2019-03-12 12:27:43 -0700

    fixed bug with progress hud textlabels

--- ARMDevSuite/Classes/ProgressHud/ARMBubbleProgressHud.swift
+++ ARMDevSuite/Classes/ProgressHud/ARMBubbleProgressHud.swift
@@ -617,12 +617,12 @@ public class ARMBubbleProgressHud: UIView {
         
         titleLabel.sizeToFit()
         detailLabel.sizeToFit()
-        detailLabel.frame.size = CGSize(width: detailLabel.frame.width, height: max(LogicSuite.getLineHeight(for: detailFont), detailLabel.frame.height))
+        detailLabel.frame.size = CGSize(width: detailLabel.frame.width, height: max(UISuite.getLineHeight(for: detailFont), detailLabel.frame.height))
         
         contentView.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: indicatorView.frame.height + titleLabel.frame.height + detailLabel.frame.height + 2 * .padding)
         
-        titleLabel.frame = LayoutManager.belowCentered(elementAbove: indicatorView, padding: .padding, width: titleLabel.frame.width, height: titleLabel.frame.height)
-        detailLabel.frame = LayoutManager.belowCentered(elementAbove: titleLabel, padding: .padding, width: self.frame.width - 6 * .padding, height: detailLabel.frame.height)
+        titleLabel.frame = LayoutManager.belowCentered(elementAbove: indicatorView, padding: 0, width: titleLabel.frame.width, height: titleLabel.frame.height)
+        detailLabel.frame = LayoutManager.belowCentered(elementAbove: titleLabel, padding: 0, width: self.frame.width - 6 * .padding, height: detailLabel.frame.height)
         
         contentView.center = self.center
         
